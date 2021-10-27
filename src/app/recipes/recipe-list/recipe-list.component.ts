ae3c01bda9d210110811536049c7f577ae05618e
System Administrator <root@Tests-MacBook-Pro.local>
2021-10-27 16:02:02 +0200

    section 4 done

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'Just a test recipe', 'https://i.pinimg.com/564x/8d/c5/31/8dc531fd1168a51c9e5bc9aca0d45051.jpg'),
+    new Recipe('A test recipe', 'Just a test recipe', 'https://i.pinimg.com/564x/8d/c5/31/8dc531fd1168a51c9e5bc9aca0d45051.jpg'),
+  ];
   constructor() { }
-
+  
   ngOnInit(): void {
   }
 
