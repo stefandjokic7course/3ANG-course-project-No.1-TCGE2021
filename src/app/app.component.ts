974c72bbdc50752f9debb8e2b8d0c383f9dadf02
System Administrator <root@Tests-MacBook-Pro.local>
2021-11-02 08:46:52 +0100

    Section 6.2 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'course-project-No1';
+  featureSelected: string = 'recipe';
+
+  onNavigate(feature: string) {
+    console.log(feature);
+    this.featureSelected = feature;
+  }
 }
